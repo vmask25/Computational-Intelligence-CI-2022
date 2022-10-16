006f15a0d5b3f0a42bea704eb2e73fa3c187ad25
Simone Mascali s305001 <s305001@studenti.polito.it>
2022-10-16 23:59:19 +0200

    updates

--- lab1/README.md
+++ lab1/README.md
@@ -1,1 +1,30 @@
-Laboratory 1
+# Laboratory 1 : Set Covering
+
+## Task
+
+Given a number N and some lists of integers $P = (L_0, L_1, L_2, ..., L_n)$, determine, if possible, $S=(L_{s_0}, L_{s_1}, ..., L_{s_n})$ such that each number between 0 and N-1 appears in at least one list
+$$\forall n \in [0, N-1] \ \exists i : n \in L_{s_i}$$
+and that the total numbers of elements in all $L_{s_i}$ is minimum.
+
+## Explanation
+
+The first used algorithm is the greedy solution given by the Professor.
+The second solution uses the combinations inside a loop, generating for each level all the possible combinations (without repetitions).
+Once found a solution, it explores the subsequent level in order to be sure not to found another solution, and then it stops, returning the optimal solution.
+
+| N | Processed nodes | Number of Elements (weight) |
+|---|---|---|
+|5      |2625|5|
+|10     |20875|10|
+|20     |331211|23|
+|100    |-|-|
+|500    |-|-|
+|1000   |-|-|
+
+Given that this is an uninformed strategy, the number of processed nodes can be higher and in fact from N=50 it starts to take too long to find the optimal solution.
+There is something that could minimize the number of processed nodes, such as A*.
+
+## Contributors
+
+- [Marco Sacchet](https://github.com/saccuz)
+- [Fabrizio Sulpicio](https://github.com/Xiusss)
--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,8 +1,10 @@
 import random
-from copy import copy
-import numpy as np
 from itertools import combinations
+import logging
 
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+# List of lists generator
 def problem(N, seed=None):
     random.seed(seed)
     return [
@@ -12,68 +14,66 @@ def problem(N, seed=None):
 
 nodes = 0
 
+# Professor's greedy search
+def greedy_search(space, N):
+    goal = set(range(N))
+    covered = set()
+    solution = list()
+    while goal != covered:
+        x = set(space.pop(0))
+        if not x < covered: 
+            solution.append(x)
+            covered |= x 
+    return solution
+
+# Utility function to check the solution of the Breadth-First search
 def check_solution(solution, N):
     if len(solution) == 0:
-        return (False, solution, 0)
-    check_ = np.ones(N)*(N+1) # array of length N, if solution is correct, every element will have value equals his index
-    collision = np.zeros(N)
-    for el in solution:
-        for i in el:
-            if check_[i] == i: # mark collision if already present
-                collision[i] = 1
-            else:
-                check_[i] = i
-    if any(j == N+1 for j in check_): # check that every element has been set
-        return (False, solution, np.sum(collision))
+        return (False, solution)
+    check_ = set(range(N)) # create a set with all the number of the requested solution
+    sol = set()
+    for e in solution:
+        sol |= set(e)  # add in the set every number of the possible solution
+    if sol == check_: #check if the solution is equal to the request, not a subset nor a overset
+        return (True, solution)
     else:
-        return (True, solution, np.sum(collision))
+        return (False, solution)
 
-
-def tree_explorer_DF(sol, space, N):
-    """ Function that check if exists at least a solution, but not guarantees it is the best one """
-    local_s = copy(sol)
-    isCorrect = check_solution(sol, N)
-    if  len(local_s) != 0 and len(local_s[-1]) <= isCorrect[2]: # check number of collision less or equal of list's length
-        sol.pop(-1)
-    if isCorrect[0]:
-        return isCorrect
-    for l in space:
-        local_s.append(l) # adding a new element in the possible solution
-
-        global nodes
-        nodes += 1
-
-        ind_l = space.index(l)
-        ctrl = tree_explorer_DF(local_s, space[ind_l+1 : ], N)
-        if ctrl is not None and ctrl[0]:
-            if nodes != 0:
-                print(f"Visited nodes with partial DF: {nodes}")
-                nodes = 0
-            return ctrl
-
+# Breadth-First search
 def tree_explorer_BF(space, N):
     """ Function that produce the best solution. It can be computationally too massive """
-    nodes = 0
-    for i in range(N):
+    level = 0
+    nodes = []
+    solution = list()
+    for i in range(1,N):
+        nod = 0
         for e in combinations(space, i): # create every possible combination of element using i elements
-            nodes += 1
+            nod += 1
             isCorrect = check_solution(e, N)
             if isCorrect[0]:
-                print(f"Visited nodes with BF: {nodes}")
-                return isCorrect #if the solution is correct, it's also the best solution, because is the first I can find with minimum length
-    return (False, [], 0)
+                if len(solution) == 0 or sum(len(e) for e in solution[1]) > sum(len(e) for e in isCorrect[1]): # check if the solution found is better than the already existing solution
+                    solution = isCorrect
+                    level = i
+        nodes.append(nod)
+        if level != 0 and level+1 <= i:
+            break
+    if len(solution) != 0:
+        nodes.pop(-1)
+        logging.info(f"Visited nodes with BF: {sum(nodes)}")
+        return solution
+    return (False, [])
 
 
 seed = 42
-
-for N in [5, 10, 20, 100, 500, 1000]:
-    space = problem(N, seed)
+    
+for N in [5,10,20,100,500,1000]:
+    space = problem(N,seed)
     space.sort(key=len)
-    solution = tree_explorer_DF([], space, N)
-    print(f"Possible solution for {N}: {len(solution[1])} with {sum(len(e) for e in solution[1])} elements")
+    solution = greedy_search(space, N)
+    logging.info(f"Existing solution for {N}: {len(solution)} with (weight) {sum(len(e) for e in solution)} elements\n")
 
 for N in [5, 10, 20, 100, 500, 1000]:
     space = problem(N, seed)
     space.sort(key=len)
     solution = tree_explorer_BF(space, N)
-    print(f"Optimal solution for {N}: {len(solution[1])} with {sum(len(e) for e in solution[1])} elements")
+    logging.info(f"Optimal solution for {N}: {len(solution[1])} lists {solution[1]}  with (weight) {sum(len(e) for e in solution[1])} elements\n")
