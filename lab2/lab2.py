985a1d0ff26a44fdb2785119773591fad151a4fb
Simone Mascali s305001 <s305001@studenti.polito.it>
2022-11-02 20:48:00 +0100

    First version of lab2 (still to improve)

---
+++ lab2/lab2.py
@@ -0,0 +1,173 @@
+import random
+from itertools import combinations
+import logging
+import numpy as np
+
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+NUM_GENERATIONS = 500
+
+N=20
+
+POPULATION_SIZE = 70        #100 o 70
+OFFSPRING_SIZE = 70         #40
+
+#PROBLEMS: 
+# -con valori diversi di popolazione e offspring size non trovava soluzioni valide, arrivavamo a elitarismo di soluzioni non valide,
+#     forse non stiamo dando il giusto peso alle cose, il fatto di trovare una soluzione valida (che copra tutti gli N numeri) non viene sempre
+#     rispettata (rivedi fitness function con quelle proporzioni)
+# -rivedere la fitness function
+
+# List of lists generator
+def problem(N, seed=None):
+    random.seed(seed)
+    return [
+        list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
+        for n in range(random.randint(N, N * 5))
+    ]
+
+nodes = 0
+
+seed = 42
+
+space = set(tuple(sorted(set(_))) for _ in problem(N, seed))
+print(f"The search space is: \n{space}")
+space = list(_ for _ in space)
+
+PROBLEM_SIZE = len(space)
+
+def collisions(sol):
+    check_ = np.ones(N)*(N+1) #vettore di lunghezza N, se la soluzione e' giusta ogni elemento avra' valore pari al suo indice
+    collision = 0
+    for el in sol:
+        for i in el:
+            if check_[i] == i: #se il valore e' gia' presente segno la collisione
+                collision += 1
+            else:
+                check_[i] = i
+    return collision
+
+def fitness(genome, space):
+    sol = list()
+    for i, _ in enumerate(genome):
+        if _:
+            sol.append(space[i])
+    #print(sol)
+    collisions_ = collisions(sol)
+    #print(collisions_)
+
+    check_set = set()
+    for e in sol:
+        check_set |= set(e)
+    how_many_covered = len(check_set)
+    #print(check_set)
+    #print(f"len : how_many_covered")
+    #if how_many_covered == N:
+    #    return -((collisions_))   # + (N - how_many_covered)*0.1)#-collisions_
+    #else:
+    #    return -((collisions_)*0.1 + (N - how_many_covered)*0.9)
+    fit= -((collisions_)*0.1 + (N - how_many_covered)*0.9)  # da RIVEDERE, come dai più priorità al numero di numeri coperti?
+    #    # dubbio: con numeri grandi cosa succede?
+    ##fit= -((collisions_) + (N - how_many_covered))
+    return fit
+
+def tournament(population, tournament_size=2):
+    return max(random.choices(population, k=tournament_size), key=lambda i: fitness(i, space))
+
+def mutation(g):
+    point = random.randint(0, PROBLEM_SIZE - 1)
+    # Reversing one of the 1/0 in a random point of the individual/genome
+    #return g[:point] + (1 - g[point],) + g[point + 1 :]
+    mutated = g[:point] + (1 - g[point],) + g[point + 1 :]
+    point = random.randint(0, PROBLEM_SIZE - 1)
+    # Reversing one of the 1/0 in a random point of the individual/genome
+    return mutated[:point] + (1 - mutated[point],) + mutated[point + 1 :]
+
+def cross_over(g1, g2):
+    cut = random.randint(0, PROBLEM_SIZE)
+    return g1[:cut] + g2[cut:]
+
+#for N in [5]:#, 10, 20, 100, 500, 1000]:
+
+#new_space = list()
+#for _tuple in space:
+#    _list = list()
+#    for _el in _tuple:
+#        _list.append(_el)
+#    new_space.append(_list)
+#print(new_space)
+#print(len(new_space))
+#
+#fitness([1, 1, 0, 0, 0, 0, 0, 1, 0, 0], space)
+
+population = list()
+for genome in [tuple([random.choice([1, 0]) for _ in range(PROBLEM_SIZE)]) for _ in range(POPULATION_SIZE)]:
+    population.append(genome)
+
+population = sorted(population, key=lambda i: fitness(i,space), reverse=True)
+#for _ in population:
+#    print(f"{_}, {fitness(_, space)}")
+
+exit_ = 0
+for g in range(NUM_GENERATIONS):
+    offspring = list()
+    print(g)
+    for i in range(OFFSPRING_SIZE):
+        if random.random() < 0.3:
+            p = tournament(population)
+            o = mutation(p)
+        else:
+            p1 = tournament(population)
+            p2 = tournament(population)
+            o = cross_over(p1, p2)
+        f = fitness(o, space)
+        offspring.append(o)
+            
+        ############################################
+        sol_intermedia = list()
+        for i, _ in enumerate(population[0]):
+            if _:
+                sol_intermedia.append(space[i])
+        check_set = set()
+       
+        for e in sol_intermedia:
+            check_set |= set(e)
+        how_many_covered = len(check_set)
+        if N==20 and how_many_covered == 20 and collisions(sol_intermedia) == 3: 
+            exit_ = 1
+            print(f"FOUND at {g+1}")
+            break
+        if N==10 and how_many_covered == 10 and collisions(sol_intermedia) == 0: 
+            exit_ = 1
+            print(f"FOUND at {g+1}")
+            break
+        if N==5 and how_many_covered == 5 and collisions(sol_intermedia) == 0: 
+            exit_ = 1
+            print(f"FOUND at {g+1}")
+            break
+        ############################################
+    population += offspring
+    population = sorted(population, key=lambda i: fitness(i,space), reverse=True)[:POPULATION_SIZE]
+    if exit_:
+        break
+
+############################################
+#for _ in population:
+#    print(f"{_}, {fitness(_, space)}")
+
+
+sol_fin = list()
+for i, _ in enumerate(population[0]):
+    if _:
+        sol_fin.append(space[i])
+
+
+print(f"Best solution up to now ({NUM_GENERATIONS} generations): {sol_fin}")
+check_set = set()
+for e in sol_fin:
+    check_set |= set(e)
+how_many_covered = len(check_set)
+print(f"How many covered: {how_many_covered}")
+print(f"Collisions: { collisions(sol_fin)}")
+print(f"Weight: {sum(len(_) for _ in sol_fin)}")
+############################################
