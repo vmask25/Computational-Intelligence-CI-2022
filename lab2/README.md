6b39aafdf54180696e2e19ddf84446fbeabcba95
Simone Mascali s305001 <s305001@studenti.polito.it>
2022-11-05 23:48:29 +0100

    Final version of Version 1 code for lab2

---
+++ lab2/README.md
@@ -0,0 +1,41 @@
+# Lab 2: Set Covering through a genetic algorithm
+
+## Task
+Given a number $N$ and some lists of integers $P = (L_0, L_1, L_2, ..., L_n)$ determine, if possible, $S = (L_{s_0}, L_{s_1}, L_{s_2}, ..., L_{s_n})$ such that each number between $0$ and $N-1$ appears in at least one list and that the total numbers of elements in all $L_{s_i}$ is minimum.
+
+## Development
+
+### 1. Prove that a solution exists
+
+Through the analysis of the results obtained in the first lab, we know that a solution exists. We are committed in finding the optimal one with a wide problem space, focusing on performance with greater values of N (like 2000, 5000 and 10000). 
+
+### 2. Find the Optimal Solution
+We have two versions of the algorithm, in this repository there is the Version 1, and in the other teammates' repository there is the Version 2.\
+In both cases, to find a good solution, we opted for a genetic algorithm where we represent each genome as a bitmap of 0's and only one "1" randomly chosen at the beginning to initialize the population. 
+
+## Version 1
+In each generation we take one of two possible paths: 
+- the first, with a probability of 30%, in which we select a parent through a tournament of size 2 (tau), and then we mutate the winner parent to obtain the offspring;
+- the second, with a probability of 70%, in which we select two parents through two different torunaments of size 2, and then we cross-over these two parents to obtain the offspring;
+
+At the end of each generation we add the generated offsprings to the population, and then we cut off the worst genomes to return back to the initial population size, ready to face the next generation.\
+The cross-over strategy we use composes the result picking a slice of random dimension from each of the parents, so it's a very basic one, but it still gives good results.\
+The tournament is a basic tournament picking two parents at random inside the population and making them "fight" comparing their fitness.\
+To determine the fitness of a potential solution we calculate the amount of covered numbers, and then we calculate the amount of collisions (duplicates) in the current solution.\
+With each generation we try to maximize the covered numbers between 0 and N-1 at first, and then we try to minimize the amount of collisions (maximizing -collisions).\
+To prevent elitarism we apply a double mutation (two "bit tilts") with a rate of 30% as cited before.\
+With a reasonable time we achieve good results for very high values of N.
+
+Comparing to lab1, we succedeed in calculating the solution for $N$'s bigger than 20 reaching N=10000 in a quite reasonable time (it strongly depends on number of generations, and population and offspring sizes).
+
+Here is reported a table in which we report the weight results for different values of N, changing the population and offspring sizes, and comparing the Version 1 with the Version 2 of the algorithm, including also the bloatings and mean bloatings.
+
+![table](./results.PNG "Results")
+
+## Improvements
+We are sure that some improvements in the logic of the genetic algorithm are possible, some improving ideas we had are only cited in comments directly in the code, but we are open to suggestions and critics about our versions.
+
+## Contributors
+
+- [Marco Sacchet](https://github.com/saccuz)
+- [Fabrizio Sulpizio](https://github.com/Xiusss)
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,24 +1,14 @@
 import random
-from itertools import combinations
 import logging
 import numpy as np
-# just for debugging
 import time
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 
-NUM_GENERATIONS = 5000
+NUM_GENERATIONS = 1000
 
-N=1000
-
-POPULATION_SIZE = 30        #100 o 70
-OFFSPRING_SIZE = 20         #40    (5 e 3, 30 e 20, 70 e 70, 70 e 40, 150 e 100)
-
-#PROBLEMS: 
-# OLD-con valori diversi di popolazione e offspring size non trovava soluzioni valide, arrivavamo a elitarismo di soluzioni non valide,
-#     forse non stiamo dando il giusto peso alle cose, il fatto di trovare una soluzione valida (che copra tutti gli N numeri) non viene sempre
-#     rispettata (rivedi fitness function con quelle proporzioni)
-# OLD-rivedere la fitness function
+POPULATION_SIZE = 30        
+OFFSPRING_SIZE =  20        # Other possible values (5 and 3, 30 and 20, 70 and 70, 70 and 40, 150 and 100 and others)
 
 # List of lists generator
 def problem(N, seed=None):
@@ -28,58 +18,47 @@ def problem(N, seed=None):
         for n in range(random.randint(N, N * 5))
     ]
 
-seed = 42
-
-space = list(set(tuple(sorted(set(_))) for _ in problem(N, seed)))
-
-print(f"The search space is: \n{space}")
-
-PROBLEM_SIZE = len(space)
-
+# Counts the number of collisions in the current solution (duplicated covered numbers)
 def collisions(sol):
-    check_ = np.ones(N)*(N+1) #vettore di lunghezza N, se la soluzione e' giusta ogni elemento avra' valore pari al suo indice
+    check_ = np.ones(N)*(N+1) 
     collision = 0
     for el in sol:
         for i in el:
-            if check_[i] == i: #se il valore e' gia' presente segno la collisione
+            if check_[i] == i: 
                 collision += 1
             else:
                 check_[i] = i
     return collision
 
-def fitness(genome, space):
+# Translates the bitmap into the actual list of lists taken in the current solution
+def bitmap_to_search_space(genome, space):
     sol = list()
     for i, _ in enumerate(genome):
         if _:
             sol.append(space[i])
-    collisions_ = collisions(sol)
+    return sol
 
+# Returns the amount of numbers covered by the current solution
+def covered_numbers(sol):
     check_set = set()
     for e in sol:
         check_set |= set(e)
-    how_many_covered = len(check_set)
-    
-    #if how_many_covered == N:
-    #    return -((collisions_))   # + (N - how_many_covered)*0.1)#-collisions_
-    #else:
-    #    return -((collisions_)*0.1 + (N - how_many_covered)*0.9)
-    
-    
-    #fit= -((collisions_)*0.1 + (N - how_many_covered)*0.9)  # da RIVEDERE, come dai più priorità al numero di numeri coperti?
-    
-    fit = (how_many_covered, -collisions_)
-    
-    
-    #    # dubbio: con numeri grandi cosa succede?
-    ##fit= -((collisions_) + (N - how_many_covered))
-    return fit
+    return len(check_set)
 
+# Fitness function
+def fitness(genome, space):
+    sol = bitmap_to_search_space(genome, space)
+    collisions_ = collisions(sol)
+    how_many_covered = covered_numbers(sol)
+    return (how_many_covered, -collisions_)
+
+# Simple tournament function (size (tau) =2, with a greater tau it didn't seem to improve solutions)
 def tournament(population, tournament_size=2):
     return max(random.choices(population, k=tournament_size), key=lambda i: fitness(i, space))
 
-# The mutation is made by 2 bit tilts, so two lists between the current genome lists are taken/untaken
+# The mutation is made by 2 bit tilts, so two lists between the current genome lists are taken/untaken.
+# Improving ideas: instead of 2 bit tilts, a proportional (to the problem size (genome length)) number of bit tilts could improve the solution faster.
 def mutation(g):
-    #Marco
     point1 = random.randint(0, PROBLEM_SIZE - 1)
     # Reversing one of the 1/0 in a random point of the individual/genome
     mutated = g[:point1] + (1 - g[point1],) + g[point1 + 1 :]
@@ -94,15 +73,6 @@ def mutation(g):
     # Reversing one of the 1/0 in a random point of the individual/genome
     return mutated[:point2] + (1 - mutated[point2],) + mutated[point2 + 1 :]
 
-    #Simo
-    #point = random.randint(0, PROBLEM_SIZE - 1)
-    ## Reversing one of the 1/0 in a random point of the individual/genome
-    ##return g[:point] + (1 - g[point],) + g[point + 1 :]
-    #mutated = g[:point] + (1 - g[point],) + g[point + 1 :]    
-    #point = random.randint(0, PROBLEM_SIZE - 1)
-    ## Reversing one of the 1/0 in a random point of the individual/genome
-    #return mutated[:point] + (1 - mutated[point],) + mutated[point + 1 :]
-
 # The xover is a basic xover in which the result is composed by a slice of each of the two parents.
 def cross_over(g1, g2):
     cut = random.randint(0, PROBLEM_SIZE)
@@ -111,87 +81,76 @@ def cross_over(g1, g2):
 
 ########## Main
 
-# The initial population is randomly created with bitmaps of 0's and only one "1" randomly choosen
-population = list()
-for genome in [tuple([0 for _ in range(PROBLEM_SIZE)]) for _ in range(POPULATION_SIZE)]:
-    point = random.randint(0, PROBLEM_SIZE - 1)
-    mutated = genome[:point] + (1,) + genome[point + 1 :]
-    population.append(mutated)
+for N in [5,10,20,22,50,100,500,1000,2000,5000,10000]:
+    seed = 42
+    
+    print (f"------------------------------------------Now trying to solve for N = {N}")
 
-# Initial population printing
-#for _ in population:
-#    print(_, "\n")
+    start = time.time()
 
-population = sorted(population, key=lambda i: fitness(i,space), reverse=True)
+    space = list(set(tuple(sorted(set(_))) for _ in problem(N, seed)))
 
-mutation_rate = 0.3
-exit_ = 0 
-last_10_fittest = list()
-for g in range(NUM_GENERATIONS):
-    offspring = list()
-    for i in range(OFFSPRING_SIZE):
-        if random.random() < mutation_rate:
-            p = tournament(population)
-            o = mutation(p)
-        else:
-            p1 = tournament(population)
-            p2 = tournament(population)
-            o = cross_over(p1, p2)
-        f = fitness(o, space)
-        offspring.append(o)
-            
-        ############################################
-        #sol_intermedia = list()
-        #for i, _ in enumerate(population[0]):
-        #    if _:
-        #        sol_intermedia.append(space[i])
-        #check_set = set()
-        #
-        #for e in sol_intermedia:
-        #    check_set |= set(e)
-        #how_many_covered = len(check_set)
-        #if N==20 and how_many_covered == 20 and collisions(sol_intermedia) == 3: 
-        #    exit_ = 1
-        #    print(f"FOUND at {g+1}")
-        #    break
-        #if N==10 and how_many_covered == 10 and collisions(sol_intermedia) == 0: 
-        #    exit_ = 1
-        #    print(f"FOUND at {g+1}")
-        #    break
-        #if N==5 and how_many_covered == 5 and collisions(sol_intermedia) == 0: 
-        #    exit_ = 1
-        #    print(f"FOUND at {g+1}")
-        #    break
-        ############################################
-    population += offspring
-    population = sorted(population, key=lambda i: fitness(i,space), reverse=True)[:POPULATION_SIZE]
-    fittest = fitness(population[0], space)
-    #last_10_fittest.append(fittest)
-    #if len(last_10_fittest) == 10 and not len(set(last_10_fittest)) == 1:
-    #    last_10_fittest.pop(0)    
-    #if len(last_10_fittest) == 10 and len(set(last_10_fittest)) == 1:
-    #    last_10_fittest = list()
-    #    if mutation_rate < 0.8:
-    #        mutation_rate = mutation_rate + 0.1
-    print(fittest, g+1, mutation_rate)
-    if exit_:
-        break
+    PROBLEM_SIZE = len(space)
+    fitness_calls = 0
+    # The initial population is randomly created with bitmaps of 0's and only one "1" randomly choosen
+    population = list()
+    for genome in [tuple([0 for _ in range(PROBLEM_SIZE)]) for _ in range(POPULATION_SIZE)]:
+        # Improving ideas: having a proportional (to the problem size (genome length)) number of initially taken lists could allow a faster optimization
+        # for _ in range(int(PROBLEM_SIZE*0.1)):
+        point = random.randint(0, PROBLEM_SIZE - 1)
+        mutated = genome[:point] + (1,) + genome[point + 1 :]
 
-############################################ STATISTICS and RESUME PRINTING
-sol_fin = list()
-for i, _ in enumerate(population[0]):
-    if _:
-        sol_fin.append(space[i])
+        population.append(mutated)
 
-check_set = set()
-for e in sol_fin:
-    check_set |= set(e)
-how_many_covered = len(check_set)
-print(f"Best solution up to now ({NUM_GENERATIONS} generations): {sol_fin}")
-print(f"How many covered: {how_many_covered}")
-print(f"Collisions: { collisions(sol_fin)}")
-print(f"Weight: {sum(len(_) for _ in sol_fin)}")
-print(f"Population size: {POPULATION_SIZE}")
-print(f"Offspring size: {OFFSPRING_SIZE}")
-print(f"Generations: {NUM_GENERATIONS}")
-############################################
+    population = sorted(population, key=lambda i: fitness(i,space), reverse=True)
+    fitness_calls += len(population)
+    mutation_rate = 0.3
+    #last_10_fittest = list()
+    for g in range(NUM_GENERATIONS):
+        offspring = list()
+        for i in range(OFFSPRING_SIZE):
+            if random.random() < mutation_rate:
+                p = tournament(population)
+                o = mutation(p)
+            else:
+                p1 = tournament(population)
+                p2 = tournament(population)
+                o = cross_over(p1, p2)
+            offspring.append(o)
+                
+        population += offspring
+        population = sorted(population, key=lambda i: fitness(i,space), reverse=True)[:POPULATION_SIZE]
+        fitness_calls += len(population)
+        fittest = fitness(population[0], space)
+        if g in [NUM_GENERATIONS//4, NUM_GENERATIONS//2,NUM_GENERATIONS-NUM_GENERATIONS//4, NUM_GENERATIONS-1]:
+            print(f"Status: {100 * (g+1) // NUM_GENERATIONS}%\t- Fit: {fittest}")
+        fitness_calls += 1
+
+        # This was an attempt to avoid the flatness increasing the mutation rate if the fittest individuals in the last
+        # 10 generation didn't evolved anything, but it didn't make such great changes, maybe an improvable idea.
+        #last_10_fittest.append(fittest)
+        #if len(last_10_fittest) == 10 and not len(set(last_10_fittest)) == 1:
+        #    last_10_fittest.pop(0)    
+        #if len(last_10_fittest) == 10 and len(set(last_10_fittest)) == 1:
+        #    last_10_fittest = list()
+        #    if mutation_rate < 0.8:
+        #        mutation_rate = mutation_rate + 0.1
+        #print(fittest, g+1, mutation_rate)
+
+    ############################################ STATISTICS and RESUME PRINTING
+    sol = bitmap_to_search_space(population[0], space)
+    collisions_ = collisions(sol)
+    how_many_covered = covered_numbers(sol)
+
+    end = time.time()
+
+    #print(f"Best solution up to now ({NUM_GENERATIONS} generations): {sol}")
+    print(f"How many covered: {how_many_covered}")
+    print(f"Collisions: { collisions_}")
+    print(f"Weight: {sum(len(_) for _ in sol)}")
+    print(f"Population size: {POPULATION_SIZE}")
+    print(f"Offspring size: {OFFSPRING_SIZE}")
+    print(f"Generations: {NUM_GENERATIONS}")
+    print(f"Fittness calls: {fitness_calls}")
+    print(f"Computational time: {(end - start)//60} mins and {(end-start)-(((end - start)//60)*60)} secs")
+    ############################################
